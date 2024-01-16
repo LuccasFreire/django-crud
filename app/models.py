0c2062926400763800bed20ba196aab310d45ab7
Lucas Freire <lucasfreiredev1@gmail.com>
2024-01-15 23:47:10 -0300

    Created car model, created form

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cars(models.Model):
+  model = models.CharField(max_length = 150)
+  brand = models.CharField(max_length = 50)
+  year = models.IntegerField(max_length = 150)
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from app.forms import CarsForm
 
 def home(request):
   return render(request, 'index.html')
+
+def create(request):
+  data = {}
+  data['form'] = CarsForm()
+  return render(request, 'form.html', data)

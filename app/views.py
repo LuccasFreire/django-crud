c7dc967c0a453f37276d8c1061a3555e94f637e7
Lucas Freire <lucasfreiredev1@gmail.com>
2024-02-13 02:02:30 -0300

    added paginator to home view, added paginator to index.html

--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from app.forms import CarsForm
 from app.models import Cars
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+from django.core.paginator import Paginator
 
 def home(request):
   data = {}
-  data['db'] = Cars.objects.all()
+  all = Cars.objects.all()
+  paginator = Paginator(all,2)
+  pages = request.GET.get('page')
+  data['db'] = paginator.get_page(pages)
   return render(request, 'index.html', data)
 
 def createform(request):
@@ -14,13 +21,29 @@ def createform(request):
   data['form'] = CarsForm()
   return render(request, 'form.html', data)
 
+def isajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+
+#def create(request):
+#  form = CarsForm(request.POST or None)
+#  if form.is_valid():
+#    form.save()
+#    return redirect('home')
+#  else:
+#    print(form.errors.as_data())
+
 def create(request):
-  form = CarsForm(request.POST or None)
-  if form.is_valid():
-    form.save()
-    #form = CarsForm
+    if request.method == 'POST':
+        form = CarsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CarsForm()
+
     return render(request, 'form.html', {'form': form})
-  
+
 def view(request, pk):
   data = {}
   data['db'] = Cars.objects.get(pk=pk)
